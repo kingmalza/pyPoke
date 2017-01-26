84263a7159a94b52d0618be5cb4e1d164949086a
kingmalza <alessandro.malzanini@gmail.com>
2017-01-26 15:09:09 +0100

    KM Interface modified and tor process emprovements

--- SockController.py
+++ SockController.py
@@ -12,6 +12,8 @@ from urllib.parse import urlparse
 #from fake_useragent import UserAgent
 import stem.process
 from stem.util import term
+from stem import Signal
+from stem.control import Controller
 from bs4 import BeautifulSoup
 
 
@@ -19,16 +21,19 @@ class texc(Exception): pass
 
 class Wconnector:
 	
-	
-	tor_process = None
+	tor_process = False
+	lbrowser = ['open -a /Applications/Google\ Chrome.app %s','open -a /Applications\ Firefox.app %s']
 	
 	def __init__(self,lurl):
 		self.lurl = lurl
 		self.data = []
-
+		self.cicle = 0
+		self.maxcicle = random.randint(1,6)
+		self.browser = None
+	
+	
 	def fakereq(self,url):
 		
-
 		#return response.content
 		html_page = urlopen(url)
 		soup = BeautifulSoup(html_page, "lxml")
@@ -45,26 +50,34 @@ class Wconnector:
 				self.fakereq(url)
 
 		
-		
-		
 	def gestbrowser(self,l1):
 		
 		timeop = random.randint(5,50)
-		print("Now we process %s for time %s" % (l1,timeop))
+		print("Now we process %s for time %s (Cycle: %s -> %s)" % (l1,timeop,self.cicle,self.maxcicle))
 
 		try:
-			webbrowser.get("open -a /Applications/Google\ Chrome.app %s").open(l1)	
-			time.sleep(timeop) #delay of 10 seconds
+			webbrowser.get(self.browser).open(l1)	
+			time.sleep(timeop)
+			
+			#reip variable for reget new ip address from tor controller after some page visited
+			self.cicle += 1
+			if self.cicle >= self.maxcicle:
+				with Controller.from_port(port = 9051) as controller:
+					controller.authenticate()
+					controller.signal(Signal.NEWNYM)
+				self.cicle = 0
+				self.maxcicle = random.randint(1,10)
+				self.changebro()
+				
 			self.fakereq(l1)
 		except Exception as ex1:
 			print ("Exception ex1 is %s" % (str(ex1.args)))
 			self.fakereq(self.lurl)
 
-	def query(self, startT = False):
-		"""
-		Uses pycurl to fetch a site using the proxy on the SOCKS_PORT.
-		"""
-		if startT == True:
+			
+	def query(self):
+
+		if type(self).tor_process == True:
 			try:
 				type(self).starTor()
 			except Exception as texcept:
@@ -76,18 +89,24 @@ class Wconnector:
 		except texc:
 			print("Url lists cannot be blank")
 		else:
+			self.changebro()
 			self.fakereq(self.lurl)
-		
+	
+	
+	def changebro(self):
+		bro = self.browser[random.randint(1,len(type(self).lbrowser)-1)]
+		setattr(self,'browser',bro)
 
+		
 	# Start an instance of Tor configured to only exit through Russia. This prints
 	# Tor's bootstrap information as it starts. Note that this likely will not
 	# work if you have another Tor instance running.
-
 	@staticmethod
 	def print_bootstrap_lines(line):
 		if "Bootstrapped " in line:
 			print(term.format(line, term.Color.BLUE))
 
+			
 	@classmethod
 	def starTor(cls):
 		SOCKS_PORT = 7000
@@ -102,6 +121,7 @@ class Wconnector:
 		init_msg_handler = cls.print_bootstrap_lines,
 		)
 	
+	
 	@classmethod
 	def stopTor(cls):
 		print(term.format("Stopping Tor:\n", term.Attr.BOLD))
@@ -112,16 +132,16 @@ class Wconnector:
 		
 if __name__ == '__main__':
 
-	print('ciao')
+	Wconnector.tor_process = True
 	w = Wconnector('http://www.sloop1.com/')
-	t = Wconnector('http://www.sloop1.com/')
+	t = Wconnector('http://www.patriziameo.it/')
 	#c = Wconnector()
 	
 	try:
     
 		print(term.format("\nChecking our endpoint:\n", term.Attr.BOLD))
-		w.query(startT = False)
-		#t.query()
+		w.query()
+		t.query()
 		#c.query()
 		
 		#print(fakereq(r"http://www.sloop1.com/about-neuromarketing-company/"))
